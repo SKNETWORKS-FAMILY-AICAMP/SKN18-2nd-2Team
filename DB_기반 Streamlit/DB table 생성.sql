c3389d4138adf5859fc14ed7f6334f2c408a88b2
chopin0807@naver.com <junho08075@gmail.com>
2025-09-05 12:18:12 +0900

    DB 기반 streamlit 데이터 파일 및 create table sql파일생성

---
+++ DB_기반 Streamlit/DB table 생성.sql
@@ -0,0 +1,16 @@
+CREATE TABLE examplesdb.Netflix_customers (
+  customer_id              INT PRIMARY KEY AUTO_INCREMENT,
+  age                      INT,
+  gender                   VARCHAR(10),
+  subscription_type        VARCHAR(20),
+  region                   VARCHAR(50),
+  device                   VARCHAR(50),
+  watch_hours              Float,
+  last_login_days          INT,
+  monthly_fee              Float,
+  payment_method           VARCHAR(20),
+  number_of_profiles       INT,
+  favorite_genre           VARCHAR(30),
+  avg_watch_time_per_day   Float,
+  churned                  TINYINT(1)
+);
